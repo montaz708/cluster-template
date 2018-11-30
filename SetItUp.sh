92077d29cf83b262d0bc868792f5f9428fedd527
montaz708 <alex.vazquez708@gmail.com>
2018-11-30 13:01:37 +0000

    Created a bash script to configure/install some dependencies

---
+++ SetItUp.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+sudo su
+
+cd ~
+
+rpm -Uvh http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+
+yum install -y munge-devel munge-libs readline-devel perl-ExtUtils-MakeMaker openssl-devel pam-devel rpm-build perl-DBI perl-Switch munge mariadb-devel
+
+wget https://download.schedmd.com/slurm/slurm-18.08.3.tar.bz2
+
+rpm -Uvh ./rpmbuild/RPMS/x86_64/*.rpm
+
+useradd slurm
+mkdir /var/log/slurm
+
+chown slurm /varlog/slurm
+
+yum install mariadb-server -y
+systemctl start mariadb
+systemctl enable mariadb
+
